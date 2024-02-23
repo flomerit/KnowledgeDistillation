7d443963505016dbb8ba463936eccc530ba25f99
flomerit <111960203+flomerit@users.noreply.github.com>
2024-02-23 21:32:22 +0800

    Update biLSTM.py
    注释，先去nn了

--- models/biLSTM.py
+++ models/biLSTM.py
@@ -11,11 +11,11 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class biLSTM(nn.Module):
+class biLSTM(nn.Module):  # nn.Module 是 PyTorch 中的一个重要基类，也是biLSTM子类的父类必须指明，用于构建神经网络模型，不可省略
 
-    def __init__(self, config):
-        super(biLSTM, self).__init__()
-        self.Embedding = nn.Embedding(21128,300)
+    def __init__(self, config):  # 参数 config 用于传递模型的超参数等配置信息
+        super(biLSTM, self).__init__()  # 调用了 biLSTM 类的父类 nn.Module 的 __init__ 方法（2.x的写法;3.x中super().__init__() 即可调用父类的初始化方法，而不需要显式地传递类名和 self 参数。）
+        self.Embedding = nn.Embedding(21128,300)   # 将离散的词汇（比如单词或者字符）映射到连续的低维向量空间中。Embedding 层的作用是将一个大小为 21128 的词汇表中的每个单词（或者标记）映射成一个 300 维的稠密向量
         self.lstm = nn.LSTM(input_size=300, hidden_size=300,
                             num_layers=1, batch_first=True, dropout=0, bidirectional=True)
         # self.linear = nn.Linear(in_features=256, out_features=2)
